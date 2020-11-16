4d2752b2b1c10ca10818606fd67c6d8710d48046
thanirin <thanirin.tri@student.mahidol.ac.th>
2020-11-16 17:07:31 +0700

    change port to 4000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 app.use(express.json());
 console.log("helo");
 app.get('/', (req, res) => res.json({message: "Hello World"}))

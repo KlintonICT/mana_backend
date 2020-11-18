3c8123d90084b0130fbbec17a35e702e57957c5f
Wipu <wipu25@gmail.com>
2020-11-18 10:20:49 +0700

    heroku test

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-app.use(express.json());
-console.log("helo");
-app.get('/', (req, res) => res.json({message: "Hello World"}))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.post("/callback", line.middleware(configFile), async (req, res) => {
+    const bodyEvents = req.body.events[0];
+    var userData = lineFunction.callback(bodyEvents);
+    console.log(bodyEvents)
+    res.json(bodyEvents); // req.body will be webhook event object
+  });
+  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(express.json());
+// console.log("helo");
+// app.get('/', (req, res) => res.json({message: "Hello World"}))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))

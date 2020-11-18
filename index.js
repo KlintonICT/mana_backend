15a6a46ffed3dc3088bbc53474161a79f4b11317
Wipu <wipu25@gmail.com>
2020-11-18 11:19:57 +0700

    heroku deploy

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ const configFile = require("./LineToken.json");
 
 app.post("/callback", line.middleware(configFile), async (req, res) => {
     const bodyEvents = req.body.events[0];
-    console.log(bodyEvents)
+    console.log(bodyEvents.message.textz)
     res.json(bodyEvents); // req.body will be webhook event object
   });
   
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(express.json());
-// console.log("helo");
-// app.get('/', (req, res) => res.json({message: "Hello World"}))
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

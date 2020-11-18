5a4a3b0bdc6386b2df44c0c24bfabd56ffb9b420
Wipu <wipu25@gmail.com>
2020-11-18 11:23:59 +0700

    heroku deploy

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const configFile = require("./LineToken.json");
 
 app.post("/callback", line.middleware(configFile), async (req, res) => {
     const bodyEvents = req.body.events[0];
-    console.log(bodyEvents.message.text)
+    console.log("Text Received : "+bodyEvents.message.text)
+    console.log("User ID       : "+bodyEvents.source.userId)
+    console.log("Chat Id       : "+bodyEvents.message.id)
     res.json(bodyEvents); // req.body will be webhook event object
   });
   

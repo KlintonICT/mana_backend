c0b0fa91f48723c5d4d413d1fe1311e54e09f5c7
Klinton Chhun <klintonchhun@Klintons-MacBook-Pro.local>
2020-11-19 18:08:13 +0700

    process env to line token

--- src/routes/line.js
+++ src/routes/line.js
@@ -3,13 +3,7 @@ import express from "express";
 import { middleware } from "@line/bot-sdk";
 import Line from "../controller/line";
 
-// import { lineToken } from "../token/line";
-
-const lineToken = {
-  channelSecret: "ddb0c4650713e7335687225ad58f9d50",
-  channelAccessToken:
-    "Y2BO2H4Sa3jkv5SRF2cDBvrf0LBb+cJwVjWTMwVaox7F0gXU3VqjT/PpLvCWaZteD1rew1Ae9XtGpsZR+6DNXva0qxwp4Fs3Tq+z4mQhCvoCwOd0YkmJKoVhhgg6dbfzRXlmfz2VqDyqntc4z3Pu9AdB04t89/1O/w1cDnyilFU=",
-};
+import { lineToken } from "../token/line";
 
 const router = express.Router();
 

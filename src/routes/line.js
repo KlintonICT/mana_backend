de30eeb2b5180336651dc2b9cf594ecab7542cd6
Klinton Chhun <klintonchhun@Klintons-MacBook-Pro.local>
2020-11-19 11:55:26 +0700

    add middelware for line

--- src/routes/line.js
+++ src/routes/line.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import line from "@line/bot-sdk";
+import { middleware } from "@line/bot-sdk";
 import Line from "../controller/line";
 
 import { lineToken } from "../token/line";
 
 const router = express.Router();
 
-router.post("/callback", line.middleware(lineToken), Line.lineCallback);
+router.post("/callback", middleware(lineToken), Line.lineCallback);
 
 export default router;
--- src/token/line.js
+++ src/token/line.js
@@ -1,4 +1,4 @@
 export const lineToken = {
-  channelSecret : process.env.channelSecret,
-  channelAccessToken : process.env.channelAccessToken
+  channelSecret : process.env.CHANNEL_SECRET,
+  channelAccessToken : process.env.CHANNEL_ACCESS_TOKEN
 }

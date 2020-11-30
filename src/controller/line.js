c5efa8dc4adbfe71f58acaf7014c9501790daa39
Klinton Chhun <klintonchhun@Klintons-MacBook-Pro.local>
2020-11-30 17:23:25 +0700

    refactor greeting message code

--- src/controller/line.js
+++ src/controller/line.js
@@ -6,31 +6,31 @@ const lineCallback = async (req, res) => {
   const bodyEvents = req.body.events[0];
   console.log("Body          : ", bodyEvents);
 
-  if(bodyEvents.source.userId != null){
+  if (bodyEvents.source.userId != null) {
     console.log("User ID       : ", bodyEvents.source.userId);
-  }  
-  if(bodyEvents.message != null){
-    const replyToken = bodyEvents.replyToken;
-    const text = bodyEvents.message.text
-    console.log("Text Received : ", text);
-    console.log("Chat ID       : ", bodyEvents.message.id);
-    if (text === "ส่งบิล") reply(replyToken,responseMessage);
   }
 
-  if(bodyEvents.type != null){
-    if(bodyEvents.type == "join"){
+  if (bodyEvents.message != null) {
+    const replyToken = bodyEvents.replyToken;
+    const text = bodyEvents.message.text;
+    console.log("Text Received : ", text);
+    console.log("Chat ID       : ", bodyEvents.message.id);
+    if (text === "ส่งบิล") reply(replyToken, responseMessage);
+  }
+
+  if (bodyEvents.type != null) {
+    if (bodyEvents.type == "join") {
       const replyToken = bodyEvents.replyToken;
       console.log("Yay joinin");
-      reply(replyToken,greetingMessage);
+      reply(replyToken, greetingMessage);
     }
   }
 
   // req.body will be webhook event object
   res.json(bodyEvents);
-
 };
 
-const reply = (replyToken,responseMessage) => {
+const reply = (replyToken, messages) => {
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.AUTHORIZATION}`,
@@ -38,7 +38,7 @@ const reply = (replyToken,responseMessage) => {
 
   const data = {
     replyToken: replyToken,
-    messages: responseMessage,
+    messages: messages,
   };
 
   axios
--- src/data/greetingMessage.js
+++ src/data/greetingMessage.js
@@ -1,21 +1,22 @@
 export const greetingMessage = [
-    {
-        "type":"text",
-        "text":"สวัสดีจ้า เราเป็น Mana แชทบอท พี่ๆ สามารถพิมพ์คำว่า “ส่งบิล” เพื่อส่งรายงานประจำวันได้เลย"
-   },
-   {
-    "type": "flex",
-    "altText": "This is a Flex Message",
-    "contents": {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://manastrgacc.blob.core.windows.net/receipt-image/2811552165845925-photo_2020-11-30%2017.00.03.jpeg",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover"
-        }
-      }
-  }
-  ];
-  
+  {
+    type: "text",
+    text:
+      "สวัสดีจ้า เราเป็น Mana แชทบอท พี่ๆ สามารถพิมพ์คำว่า “ส่งบิล” เพื่อส่งรายงานประจำวันได้เลย",
+  },
+  {
+    type: "flex",
+    altText: "This is a Flex Message",
+    contents: {
+      type: "bubble",
+      hero: {
+        type: "image",
+        url:
+          "https://manastrgacc.blob.core.windows.net/receipt-image/2811552165845925-photo_2020-11-30%2017.00.03.jpeg",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+    },
+  },
+];

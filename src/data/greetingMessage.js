75e7c221cad9846b1a3a21fa40fb5e50b92e19e1
Wi Pu <wipu25@gmail.com>
2020-11-30 14:26:18 +0700

    comment

--- src/data/greetingMessage.js
+++ src/data/greetingMessage.js
@@ -2,18 +2,6 @@ export const greetingMessage = [
     {
         type:"text",
         text: "สวัสดีจ้า เราเป็น Mana แชทบอท พี่ๆ สามารถพิมพ์คำว่า “ส่งบิล” เพื่อส่งรายงานประจำวันได้เลย"
-      },
-      {
-        type:"template",
-        altText : "คู่มือการใช้งาน",
-        template : {
-            type : "carousel",
-            columns : [
-                {
-                    thumbnailImageUrl: "https://imagelogo.co.th/wp-content/uploads/2018/07/slide-image-flex-graphic-03.png"
-                }
-            ]
-        }
       }
   ];
   

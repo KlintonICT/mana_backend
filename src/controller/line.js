fb67db724101e9275a5efee259a7601cbca8d3ae
Wi Pu <wipu25@gmail.com>
2020-11-30 13:55:57 +0700

    comment

--- src/controller/line.js
+++ src/controller/line.js
@@ -1,60 +1,35 @@
 import axios from "axios";
 import { responseMessage } from "../data/responseMessage";
+import { greetingMessage } from "../data/greetingMessage";
 
 const lineCallback = async (req, res) => {
   const bodyEvents = req.body.events[0];
-
-  // if(bodyEvents.message != null){
-  //   const text = bodyEvents.message.text
-  //   console.log("Text Received : ", text);
-  // console.log("Chat ID       : ", bodyEvents.message.id);
-  // console.log("Chat ID       : ", bodyEvents.message.id);
-  // }
-  
-
-
-  if(bodyEvents.type == "join")
-  {
-    console.log("Yay joinin");
-    const replyToken = bodyEvents.replyToken;
-    greetings(replyToken);
+  console.log("Body          : ", bodyEvents);
+  if(bodyEvents.message != null){
+  const text = bodyEvents.message.text
+  console.log("Text Received : ", text);
+  console.log("Chat ID       : ", bodyEvents.message.id);
+  console.log("Chat ID       : ", bodyEvents.message.id);
+    if (text === "ส่งบิล") reply(replyToken);
+  }
+  if(bodyEvents.type != null){
+    if(bodyEvents.type == "join"){
+     console.log("Yay joinin");
+     const replyToken = bodyEvents.replyToken;
+     greetings(replyToken,responseMessage);
+    }
   }
 
-  console.log("All :"+req);
-  console.log("Body          : ", bodyEvents);
-  //console.log("User ID       : ", bodyEvents.source.userId);
+  if(bodyEvents.source.userId != null){
+    console.log("User ID       : ", bodyEvents.source.userId);
+  }
 
   // req.body will be webhook event object
   res.json(bodyEvents);
 
-  if (text === "ส่งบิล") reply(replyToken);
 };
 
-const reply = (replyToken) => {
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.AUTHORIZATION}`,
-  };
-
-  const data = {
-    replyToken: replyToken,
-    messages: responseMessage,
-  };
-
-  axios
-    .post(`${process.env.LINE_HOST}/bot/message/reply`, data, {
-      headers: headers,
-    })
-    .then((response) => {
-      console.log("Bot reply data: ", response.data);
-      console.log("Bot reply code: ", response.statusCode);
-    })
-    .catch((error) => {
-      console.log("Bot reply error: ", error);
-    });
-};
-
-const greetings = (replyToken) => {
+const reply = (replyToken,responseMessage) => {
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.AUTHORIZATION}`,

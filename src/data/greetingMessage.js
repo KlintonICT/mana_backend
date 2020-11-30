04ffc85d6b94c746572780500d039584480e9768
Wi Pu <wipu25@gmail.com>
2020-11-30 16:15:28 +0700

    greeting text

--- src/data/greetingMessage.js
+++ src/data/greetingMessage.js
@@ -2,6 +2,16 @@ export const greetingMessage = [
     {
         type:"text",
         text: "สวัสดีจ้า เราเป็น Mana แชทบอท พี่ๆ สามารถพิมพ์คำว่า “ส่งบิล” เพื่อส่งรายงานประจำวันได้เลย"
+      },
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover"
+        }
       }
   ];
   

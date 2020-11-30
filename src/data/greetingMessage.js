d9ab249d460b0fed86e7930ce817bfc936d4d298
Wi Pu <wipu25@gmail.com>
2020-11-30 16:54:26 +0700

    greeting text

--- src/data/greetingMessage.js
+++ src/data/greetingMessage.js
@@ -1,7 +1,7 @@
 export const greetingMessage = [
     {
         "type":"text",
-        "text":"Hello, user"
+        "text":"สวัสดีจ้า เราเป็น Mana แชทบอท พี่ๆ สามารถพิมพ์คำว่า “ส่งบิล” เพื่อส่งรายงานประจำวันได้เลย"
    },
    {
     "type": "flex",

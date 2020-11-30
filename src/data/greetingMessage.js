8760c3ee4874cb7079396ece7a85e37826fe55f8
Wi Pu <wipu25@gmail.com>
2020-11-30 16:47:38 +0700

    greeting text

--- src/data/greetingMessage.js
+++ src/data/greetingMessage.js
@@ -13,11 +13,7 @@ export const greetingMessage = [
           "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
           "size": "full",
           "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "uri": "http://linecorp.com/"
-          }
+          "aspectMode": "cover"
         }
       }
   }

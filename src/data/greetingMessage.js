feac0f623b92c6640a64b044c952763956f63ed8
Wi Pu <wipu25@gmail.com>
2020-11-30 16:25:18 +0700

    greeting text

--- src/data/greetingMessage.js
+++ src/data/greetingMessage.js
@@ -1,13 +1,11 @@
 export const greetingMessage = [
-      {
-        type: "bubble",
-        hero: {
-          type: "image",
-          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover"
-        }
-      }
+    {
+        "type":"text",
+        "text":"Hello, user"
+   },
+   {
+        "type":"text",
+        "text":"May I help you?"
+   }
   ];
   

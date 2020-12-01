d7f63936aba460cb87532552462b154b223c34b8
Wi Pu <wipu25@gmail.com>
2020-12-01 13:58:11 +0700

    Change picture link

--- src/data/responseMessage.js
+++ src/data/responseMessage.js
@@ -3,19 +3,19 @@ export const responseMessage = [
     type: "flex",
     altText: "This is a Flex Message",
     contents: {
-      "type": "bubble",
-      "size": "mega",
-      "hero": {
-        "type": "image",
-        "url": "https://manastrgacc.blob.core.windows.net/static-image/9085154324516149-photo_2020-12-01%2013.18.33.jpeg",
-        "size": "full",
-        "aspectRatio": "5:4",
-        "aspectMode": "cover",
-        "action": {
-          "type": "uri",
-          "uri": "https://liff.line.me/1655240292-pqEGWAmZ"
+      type: "bubble",
+      size: "mega",
+      hero: {
+        type: "image",
+        url: "https://manastrgacc.blob.core.windows.net/static-image/9085154324516149-photo_2020-12-01%2013.18.33.jpeg",
+        size: "full",
+        aspectRatio: "5:4",
+        aspectMode: "cover",
+        action: {
+          type: "uri",
+          uri: "https://liff.line.me/1655240292-pqEGWAmZ"
         },
-        "position": "relative"
+        position: "relative"
       }
     }
   },
